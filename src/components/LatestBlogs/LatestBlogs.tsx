8b1b40d8f97e71db9526b1760ed325141843acf7
JAHID HASAN <developerjahid@gmail.com>
2024-04-23 19:09:18 +0600

    setup commit

---
+++ src/components/LatestBlogs/LatestBlogs.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LatestBlogs = () => {
+    return (
+        <div>
+            <h1>Latest blogs form blogis</h1>
+        </div>
+    );
+};
+
+export default LatestBlogs;
